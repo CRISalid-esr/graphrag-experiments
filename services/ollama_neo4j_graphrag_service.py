91933b1352d1f94d7c839d314dae4395b3ba6189
eblettery <emile.blettery@gmail.com>
2025-04-28 17:55:46 +0200

    Implement changes requested before pull

--- services/ollama_neo4j_graphrag_service.py
+++ services/ollama_neo4j_graphrag_service.py
@@ -3,8 +3,11 @@ from neo4j import GraphDatabase
 from neo4j_graphrag.llm import OllamaLLM
 from neo4j_graphrag.retrievers import Text2CypherRetriever
 from neo4j_graphrag.generation import GraphRAG
+from neo4j_graphrag.exceptions import Text2CypherRetrievalError
+from neo4j_graphrag.exceptions import RagInitializationError, LLMGenerationError
 
 from dotenv import dotenv_values
+
 
 from schemas import ChatRequest, ChatResponse
 from services.graphrag_service import GraphRagService
@@ -25,31 +28,36 @@ class OllamaNeo4jGraphRagService(GraphRagService):
         """
         last_message = request.message
 
-        reply = self.text2cypher_query_graphrag(last_message)
+        reply = self._text2cypher_query_graphrag(last_message)
 
         return ChatResponse(reply=reply)
 
-    def text2cypher_query_graphrag(self, question: str) -> str:
+    def _text2cypher_query_graphrag(self, question: str) -> str:
         """
         Queries the Neo4j database using a Text2Cypher process.
         :param question: The question to answer
         :return: The answer to the question
         """
+
         auth_params = (self.config['NEO4J_USERNAME'],self.config['NEO4J_PASSWORD'])
-        driver = GraphDatabase.driver(self.config['NEO4J_URI'],auth=auth_params)
+        with GraphDatabase.driver(self.config['NEO4J_URI'],auth=auth_params) as driver:
 
-        model_params = {"temperature": float(self.config['MODEL_TEMP'])}
-        llm = OllamaLLM(model_name=self.config['MODEL_NAME'], model_params=model_params)
+            model_params = {"temperature": float(self.config['MODEL_TEMP'])}
+            llm = OllamaLLM(model_name=self.config['MODEL_NAME'], model_params=model_params)
 
-        retriever = Text2CypherRetriever(
-            driver=driver,
-            llm=llm,
-            neo4j_schema=self.config['NEO4J_SCHEMA'],
-            #examples=examples,
-        )
-
-        rag = GraphRAG(retriever=retriever, llm=llm)
-
-        response = rag.search(query_text=question)
-        driver.close()
-        return response.answer
+            retriever = Text2CypherRetriever(
+                driver=driver,
+                llm=llm,
+                neo4j_schema=self.config['NEO4J_SCHEMA'],
+                #examples=examples,
+            )
+            try:
+                rag = GraphRAG(retriever=retriever, llm=llm)
+                response = rag.search(query_text=question).answer
+                return response
+            except RagInitializationError:
+                return 'An error occured. I can not provide the information you asked for.'
+            except LLMGenerationError:
+                return 'An error occured. I can not provide the information you asked for.'
+            except Text2CypherRetrievalError:
+                return 'An error occured. I can not provide the information you asked for.'
