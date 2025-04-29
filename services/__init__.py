17f5a84eedece0b9396adfb4e3ca0d6a2f382464
eblettery <emile.blettery@gmail.com>
2025-04-29 12:35:48 +0200

    Add VertexAI Graphrag and related libraries

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,7 @@
 from .basic_neo4j_graphrag_service import BasicNeo4jGraphRagService
 from .ollama_neo4j_graphrag_service import OllamaNeo4jGraphRagService
+from .vertexai_neo4j_graphrag_service import VertexAINeo4jGraphRagService
 from .graphrag_service import GraphRagService
 
-__all__ = ["GraphRagService", "BasicNeo4jGraphRagService", "OllamaNeo4jGraphRagService"]
+__all__ = ["GraphRagService", "BasicNeo4jGraphRagService",
+           "OllamaNeo4jGraphRagService", "VertexAINeo4jGraphRagService"]
---
+++ services/vertexai_neo4j_graphrag_service.py
@@ -0,0 +1,68 @@
+import logging
+
+from neo4j import GraphDatabase
+
+from neo4j_graphrag.llm import VertexAILLM
+from neo4j_graphrag.retrievers import Text2CypherRetriever
+from neo4j_graphrag.generation import GraphRAG
+from neo4j_graphrag.exceptions import Text2CypherRetrievalError
+from neo4j_graphrag.exceptions import RagInitializationError, LLMGenerationError
+
+from vertexai.generative_models import GenerationConfig
+
+from dotenv import dotenv_values
+
+from schemas import ChatRequest, ChatResponse
+from services.graphrag_service import GraphRagService
+
+
+class VertexAINeo4jGraphRagService(GraphRagService):
+    """
+    Neo4j GraphRAG service implementation, using a VertexAI model and a Text2Cypher approach.
+    """
+    def __init__(self):
+        self.config = dotenv_values(".env")
+
+    def run(self, request: ChatRequest) -> ChatResponse:
+        """
+        Process the chat request and return a response.
+        :param request: ChatRequest object from the client side with the question to answer
+        :return: ChatResponse object with the answer to the question
+        """
+        last_message = request.message
+
+        reply = self._text2cypher_query_graphrag(last_message)
+
+        return ChatResponse(reply=reply)
+
+    def _text2cypher_query_graphrag(self, question: str) -> str:
+        """
+        Queries the Neo4j database using a Text2Cypher process.
+        :param question: The question to answer
+        :return: The answer to the question
+        """
+
+        auth_params = (self.config['NEO4J_USERNAME'],self.config['NEO4J_PASSWORD'])
+        with GraphDatabase.driver(self.config['NEO4J_URI'],auth=auth_params) as driver:
+
+            model_name = self.config['VERTEXAI_MODEL_NAME']
+            generation_config = GenerationConfig(temperature=\
+                                                     float(self.config['VERTEXAI_MODEL_TEMP']))
+            api_key = self.config['VERTEXAI_API_KEY']
+
+            llm = VertexAILLM(model_name=model_name,
+                              generation_config=generation_config, api_key=api_key)
+
+            retriever = Text2CypherRetriever(
+                driver=driver,
+                llm=llm,
+                neo4j_schema=self.config['NEO4J_SCHEMA'],
+                #examples=examples,
+            )
+            try:
+                rag = GraphRAG(retriever=retriever, llm=llm)
+                response = rag.search(query_text=question).answer
+                return response
+            except (RagInitializationError, LLMGenerationError, Text2CypherRetrievalError) as e:
+                logging.error("RAG failure: %s", e)
+                return "Une erreur est survenue. Je ne peux pas fournir l'information demandée."
