fa57be9c3eb4dcd6b99d9a76a0f56d4393ef34e5
eblettery <emile.blettery@gmail.com>
2025-04-30 17:05:23 +0200

    Add OpenAI GraphRag Service

--- services/__init__.py
+++ services/__init__.py
@@ -3,7 +3,9 @@ from .neo4j_graphrag_service import Neo4jGraphRagService
 from .basic_neo4j_graphrag_service import BasicNeo4jGraphRagService
 from .ollama_neo4j_graphrag_service import OllamaNeo4jGraphRagService
 from .vertexai_neo4j_graphrag_service import VertexAINeo4jGraphRagService
+from .openai_neo4j_graphrag_service import OpenAINeo4jGraphRagService
 
 
 __all__ = ["GraphRagService", "Neo4jGraphRagService", "BasicNeo4jGraphRagService",
-           "OllamaNeo4jGraphRagService", "VertexAINeo4jGraphRagService"]
+           "OllamaNeo4jGraphRagService", "VertexAINeo4jGraphRagService",
+           "OpenAINeo4jGraphRagService"]
---
+++ services/openai_neo4j_graphrag_service.py
@@ -0,0 +1,40 @@
+from neo4j import GraphDatabase
+from neo4j_graphrag.llm import OpenAILLM
+
+from schemas import ChatRequest, ChatResponse
+from services.neo4j_graphrag_service import Neo4jGraphRagService
+
+
+class OpenAINeo4jGraphRagService(Neo4jGraphRagService):
+    """
+    Neo4j GraphRAG service implementation, using an OpenAI model and a Text2Cypher approach.
+    """
+
+    def run(self, request: ChatRequest) -> ChatResponse:
+        """
+        Process the chat request and return a response.
+        :param request: ChatRequest object from the client side with the question to answer
+        :return: ChatResponse object with the answer to the question
+        """
+        last_message = request.message
+
+        reply = self._text2cypher_query_graphrag(last_message)
+
+        return ChatResponse(reply=reply)
+
+    def _text2cypher_query_graphrag(self, question: str) -> str:
+        # pylint: disable=duplicate-code
+        """
+        Queries the Neo4j database using a Text2Cypher process.
+        :param question: The question to answer
+        :return: The answer to the question
+        """
+        auth_params = (self.config['NEO4J_USERNAME'], self.config['NEO4J_PASSWORD'])
+        with GraphDatabase.driver(self.config['NEO4J_URI'], auth=auth_params) as driver:
+            model_name = self.config['OPENAI_MODEL_NAME']
+            model_params={"temperature": float(self.config['OPENAI_MODEL_TEMP'])}
+
+            llm = OpenAILLM(model_name=model_name,
+                              model_params=model_params)
+
+            return self._query_rag(driver, llm, question)
