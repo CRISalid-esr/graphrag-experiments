0aa6fc8d732f892cf5b569b85c0de82cd5861e12
eblettery <emile.blettery@gmail.com>
2025-04-30 16:44:38 +0200

    Refactor by adding neo4jGraphRAGService class

--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,9 @@
+from .graphrag_service import GraphRagService
+from .neo4j_graphrag_service import Neo4jGraphRagService
 from .basic_neo4j_graphrag_service import BasicNeo4jGraphRagService
 from .ollama_neo4j_graphrag_service import OllamaNeo4jGraphRagService
 from .vertexai_neo4j_graphrag_service import VertexAINeo4jGraphRagService
-from .graphrag_service import GraphRagService
 
-__all__ = ["GraphRagService", "BasicNeo4jGraphRagService",
+
+__all__ = ["GraphRagService", "Neo4jGraphRagService", "BasicNeo4jGraphRagService",
            "OllamaNeo4jGraphRagService", "VertexAINeo4jGraphRagService"]
