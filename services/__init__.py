acd2452d614f80f06c494cbd8dddd8136901c5b3
eblettery <emile.blettery@gmail.com>
2025-05-02 18:21:32 +0200

    Add ollama langchain graphrag service

--- services/__init__.py
+++ services/__init__.py
@@ -4,8 +4,11 @@ from .basic_neo4j_graphrag_service import BasicNeo4jGraphRagService
 from .ollama_neo4j_graphrag_service import OllamaNeo4jGraphRagService
 from .vertexai_neo4j_graphrag_service import VertexAINeo4jGraphRagService
 from .openai_neo4j_graphrag_service import OpenAINeo4jGraphRagService
+from .langchain_graphrag_service import LangchainGraphRagService
+from .ollama_langchain_graphrag_service import OllamaLangChainGraphRagService
 
 
 __all__ = ["GraphRagService", "Neo4jGraphRagService", "BasicNeo4jGraphRagService",
            "OllamaNeo4jGraphRagService", "VertexAINeo4jGraphRagService",
-           "OpenAINeo4jGraphRagService"]
+           "OpenAINeo4jGraphRagService", "LangchainGraphRagService",
+           "OllamaLangChainGraphRagService"]
