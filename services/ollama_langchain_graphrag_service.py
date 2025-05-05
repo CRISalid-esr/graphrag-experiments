89e42deb31256a0afc4829b73de8765d28afc730
eblettery <35691395+eblettery@users.noreply.github.com>
2025-05-05 12:14:22 +0200

    Add Lanchain GraphRAG (#15)
    * Add ollama langchain graphrag service
    
    * Add openAI langchain graphrag service

---
+++ services/ollama_langchain_graphrag_service.py
@@ -0,0 +1,26 @@
+# pylint: disable=cyclic-import
+from langchain_ollama import ChatOllama
+from schemas import ChatRequest, ChatResponse
+from services.langchain_graphrag_service import LangchainGraphRagService
+
+class OllamaLangChainGraphRagService(LangchainGraphRagService):
+    """
+    LangChain GraphRAG service implementation, using a local Ollama model.
+    """
+
+    def run(self, request: ChatRequest) -> ChatResponse:
+        """
+        Process the chat request and return a response.
+        :param request: ChatRequest object from the client side with the question to answer
+        :return: ChatResponse object with the answer to the question
+        """
+        last_message = request.message
+        llm = ChatOllama(
+            model=self.config["OLLAMA_MODEL_NAME"],
+            temperature=self.config["OLLAMA_MODEL_TEMP"],
+            num_predict=500,
+        )
+
+        reply = self._query_rag(llm,last_message)
+
+        return ChatResponse(reply=reply)
