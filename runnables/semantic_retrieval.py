1c045308bdd88febaa233525bcdc8f1354391548
eblettery <35691395+eblettery@users.noreply.github.com>
2025-05-13 17:53:30 +0200

    Add graph traversal after semantic retrieval (#28)
    * Add graph traversal after semantic retrieval to get authors with titles
    
    * Collect authors for non llm-prettified output

--- runnables/semantic_retrieval.py
+++ runnables/semantic_retrieval.py
@@ -2,11 +2,9 @@ from langchain.schema.runnable import Runnable
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Neo4jVector
 from langchain_core.embeddings import Embeddings
-from langchain_core.language_models import BaseLanguageModel
 from langchain_core.runnables import RunnableLambda
 
 from config import config as app_config
-from prompts.vector_retrieval_prettifier_prompt import VectorRetrievalPrettifierPrompt
 from schemas import ChatRequest, ChatResponse
 
 
@@ -17,7 +15,7 @@ class SemanticRetrieval(Runnable[ChatRequest, ChatResponse]):
 
     _embedding_model: Embeddings = None
 
-    def __init__(self, llm: BaseLanguageModel):
+    def __init__(self):
 
         self.vector_retriever = Neo4jVector.from_existing_index(
             embedding=self._get_model(),
@@ -28,23 +26,25 @@ class SemanticRetrieval(Runnable[ChatRequest, ChatResponse]):
             node_label="Literal",
             text_node_property="value",
             embedding_node_property="title_embedding",
+            retrieval_query="""
+               // nodes and scores are passed from the vector retrieval step
+               WITH node AS literal, score AS similarity
+               MATCH (literal)<-[:HAS_TITLE]-(doc:Document)
+                     -[:HAS_CONTRIBUTION]->(contrib:Contribution)
+                     <-[:HAS_CONTRIBUTION]-(author:Person)
+               RETURN literal.value AS text, similarity AS score,
+                   {author_names: collect(DISTINCT author.display_name)} AS metadata
+           """
         )
 
         retriever = self.vector_retriever.as_retriever(
             search_kwargs={"k": int(app_config["LANGCHAIN_VECTOR_TOPK"])}
         )
 
-        prettify_prompt = VectorRetrievalPrettifierPrompt.from_file(
-            app_config["PRETTIFIER_PROMPT"]
-        )
-
-        prettify_chain = prettify_prompt | llm
-
         self.chain: Runnable[ChatRequest, ChatResponse] = (
                 RunnableLambda(self._convert_input)
                 | retriever
                 | RunnableLambda(self._format_docs)
-                | prettify_chain
                 | RunnableLambda(self._convert_to_response)
         )
 
@@ -62,12 +62,15 @@ class SemanticRetrieval(Runnable[ChatRequest, ChatResponse]):
     def _format_docs(docs: list) -> dict:
         if not docs:
             return {"raw": "No similar titles found."}
-        raw_text = "\n".join(f"- {doc.page_content}" for doc in docs)
-        return {"list": raw_text}
+        raw_text = "\n".join(
+            f"- {doc.page_content} de {', '.join(doc.metadata['author_names'])}"
+            for doc in docs
+        )
+        return raw_text
 
     @staticmethod
     def _convert_to_response(prettified_text: str) -> ChatResponse:
-        return ChatResponse(reply=prettified_text.content, query=None)
+        return ChatResponse(reply=prettified_text, query=None)
 
     def invoke(self, __input, config=None, **kwargs):
         return self.chain.invoke(__input, config=config)
