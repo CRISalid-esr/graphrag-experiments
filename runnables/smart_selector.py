c5f08ed6acf49888d2f6b45cf74cabe4a2bb64ee
eblettery <emile.blettery@gmail.com>
2025-05-13 11:58:19 +0200

    Implement SmartSelector

--- runnables/smart_selector.py
+++ runnables/smart_selector.py
@@ -1,7 +1,11 @@
+import json
+
 from langchain.schema.runnable import Runnable
 from langchain_core.language_models import BaseLanguageModel
 from langchain_core.runnables import RunnableLambda
 
+from config import config as app_config
+from prompts.selector_prompt import SelectorPrompt
 from schemas import ChatRequest
 
 
@@ -9,17 +13,37 @@ class SmartSelector(Runnable[ChatRequest, ChatRequest]):
     """
     A simple selector that routes requests to either Cypher or semantic retrieval
     """
+    _examples = None
 
     def __init__(self, llm: BaseLanguageModel):
-        def dummy_function(request: ChatRequest) -> ChatRequest:
-            cypher_keywords = [" graph "]
-            if any(kw in request.message.lower() for kw in cypher_keywords):
-                request.metadata["route"] = "cypher"
-            else:
-                request.metadata["route"] = "semantic"
-            return request
 
-        self.chain = RunnableLambda(dummy_function)
+        if SmartSelector._examples is None:
+            with open(app_config["SELECTOR_EXAMPLES"], 'rt', encoding='utf-8') as f:
+                SmartSelector._examples = json.load(f)
+
+        selector_prompt = SelectorPrompt.from_file(
+            app_config["SELECTOR_PROMPT"]
+        )
+
+        self.selector_chain = selector_prompt | llm
+
+        self.chain: Runnable[ChatRequest, ChatRequest] = (
+                RunnableLambda(self._convert_input)
+                | RunnableLambda(self._return_request_with_metadata)
+        )
+
+    @classmethod
+    def _convert_input(cls, chat_request: ChatRequest) -> dict:
+        return {
+            "examples": cls._examples,
+            "query": chat_request.message,
+            "request": chat_request
+        }
+
+    def _return_request_with_metadata(self, __input:dict) -> ChatRequest:
+        request = __input["request"]
+        request.metadata["route"] = self.selector_chain.invoke(__input).content
+        return request
 
     def invoke(self, __input, config=None, **kwargs):
         return self.chain.invoke(__input)
