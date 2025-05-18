fff27a37b7046e4dab9fcc7f7a65cd33372fd36e
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-05-18 15:32:11 +0200

    Create page layout

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
-import 'react-chatbot-kit/build/main.css'
+import 'react-chatbot-kit/build/main.css';
 import Chatbot from "react-chatbot-kit";
 import MessageParser from "./MessageParser";
 import ActionProvider from "./ActionProvider";
 import chatBotConfig from "./ChatBotConfig";
 
+import logo from './logo.svg';
+import gplLogo from './GPLv3_Logo.svg';
+
 function App() {
-    return (<div>
-        <Chatbot
-            config={chatBotConfig}
-            messageParser={MessageParser}
-            actionProvider={ActionProvider}
-            headerText="Prototype d'agent conversationnel sur graphe de connaissance"
-            placeholderText="Posez votre question ici"
-        />
-    </div>);
+    return (
+        <div className="App">
+            <header className="Sticky-header">
+                <img src={logo} alt="Crisalid logo" className="Header-logo"/>
+                <div className="Header-title">
+                    <strong>Consortium CRISalid</strong><br/>
+                    LLM powered Current Research Information Systems
+                </div>
+            </header>
+
+            <div className="App-body">
+                <Chatbot
+                    config={chatBotConfig}
+                    messageParser={MessageParser}
+                    actionProvider={ActionProvider}
+                    headerText="Prototype d'agent conversationnel sur graphe de connaissance"
+                    placeholderText="Posez votre question ici"
+                />
+            </div>
+
+            {/* Sticky GPL Logo in Bottom-Right */}
+            <img src={gplLogo} alt="GPLv3 Logo" className="GPL-sticky-logo"/>
+        </div>
+    );
 }
 
 export default App;
